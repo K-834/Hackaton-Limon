dc175bff42abdd57bbd94a7605a6934d8ae78d4f
Christ-byte <ofchrist2@gmail.com>
2024-06-08 21:50:16 -0500

    RobotinUtp

---
+++ src/app/authentication/Quest/page.tsx
@@ -0,0 +1,261 @@
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+import { Grid, Box, Card, Typography, Button } from "@mui/material";
+// components
+import PageContainer from "@/app/(DashboardLayout)/components/container/PageContainer";
+
+const questions = [
+  {
+    question: "Si pudieras tener un superpoder, ¿cuál elegirías?",
+    answers: [
+      "a) Crear inventos asombrosos de la nada.",
+      "b) Absorber conocimientos con solo tocar un libro.",
+      "c) Convertir tus ideas en obras de arte instantáneamente.",
+      "d) Tener la fuerza y resistencia de un superhéroe.",
+      "e) Poder hablar y entender todos los idiomas del mundo.",
+    ],
+  },
+  {
+    question: "¿Qué actividad preferirías hacer un sábado por la tarde?",
+    answers: [
+      "a) Explorar nuevas tecnologías.",
+      "b) Leer un libro o ver un documental.",
+      "c) Tomar alguna sesión de arte.",
+      "d) Practicar tu deporte preferido.",
+      "e) Hacer alguna obra social con amigos.",
+    ],
+  },
+  {
+    question: "¿Cómo prefieres aprender algo nuevo?",
+    answers: [
+      "a) Experimentando.",
+      "b) Investigando.",
+      "c) A través de proyectos creativos.",
+      "d) Con trabajo de campo.",
+      "e) Trabajando en equipo.",
+    ],
+  },
+  {
+    question: "¿Qué tipo de películas o series disfrutas más?",
+    answers: [
+      "a) Ciencia ficción y futuristas.",
+      "b) Documentales y biográficas.",
+      "c) Dramáticas y artísticas.",
+      "d) Deportivas y de acción.",
+      "e) Comedias y dramas.",
+    ],
+  },
+  {
+    question: "¿Cuál sería tu lugar de vacaciones ideal?",
+    answers: [
+      "a) Lugares de potencia tecnológica.",
+      "b) Museos o lugares históricos.",
+      "c) Ciudades con una rica escena artística y cultural.",
+      "d) Lugares para hacer hiking, climbing, etc.",
+      "e) Retiro en equipo.",
+    ],
+  },
+  {
+    question: "Si tuvieras que organizar un evento, ¿qué tipo sería?",
+    answers: [
+      "a) Un speech de tecnología.",
+      "b) Un seminario educativo con expertos.",
+      "c) Una exposición cultural.",
+      "d) Unas olimpiadas.",
+      "e) Un voluntariado.",
+    ],
+  },
+  {
+    question: "¿Con qué lema te identificas?",
+    answers: [
+      "a) “Innovar es la clave del futuro.”",
+      "b) “El conocimiento es poder.”",
+      "c) “La creatividad no tiene límites.”",
+      "d) “Sin esfuerzo, no hay éxito.”",
+      "e) “Juntos somos más fuertes.”",
+    ],
+  },
+];
+
+
+const PlantillaPregunta = () => {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null);
+
+  const handleNextQuestion = () => {
+    setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+    setSelectedAnswer(null);
+  };
+
+  const handleAnswerClick = (answer: string) => {
+    setSelectedAnswer(answer);
+  };
+
+  const currentQuestion = questions[currentQuestionIndex];
+
+  return (
+    <PageContainer title="UTEPIN" description="pagina de UTEPIN">
+      <style jsx>{`
+        .divider {
+          border-top: 1px solid #ccc;
+          margin: 2px 0;
+          width: 100%;
+          margin-top: 10px;
+        }
+      `}</style>
+
+      <Grid
+        container
+        spacing={0}
+        justifyContent="center"
+        sx={{ height: "100vh" }}
+      >
+        <Card
+          elevation={6}
+          sx={{
+            p: 4,
+            zIndex: 1,
+            width: "100%",
+            margin: "50px 260px",
+            background: "#efefef",
+            borderRadius: "10px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Grid
+            item
+            container
+            justifyContent="center"
+            alignItems="center"
+            textAlign="center"
+          >
+            <Box
+              display={"flex"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              padding={"50px"}
+              width="100%"
+              textAlign="center"
+            >
+              <img
+                src="/images/logos/utepin.png"
+                alt="ImagenUTP"
+                style={{
+                  overflow: "hidden",
+                  display: "block",
+                  marginBottom: "20px",
+                  width: "100px",
+                }}
+              />
+              <Typography
+                variant="subtitle1"
+                display={"flex"}
+                justifyItems={"center"}
+                alignItems={"center"}
+                style={{ marginTop: "0" }}
+                fontSize={"30px"}
+                marginLeft={"40px"}
+              >
+                <span
+                  style={{
+                    color: "#000",
+                    backgroundColor: "#ffffff",
+                    padding: "10px",
+                    borderRadius: "15px",
+                  }}
+                >
+                  {currentQuestion.question}
+                </span>
+              </Typography>
+            </Box>
+
+            <Grid
+              item
+              width={"100%"}
+              height={"300px"}
+              marginBottom={"40px"}
+              margin={"0px 90px 0px 90px"}
+              display={"flex"}
+              flexDirection={"column"}
+            >
+              {currentQuestion.answers.map((answer, idx) => (
+                <Button
+                  key={idx}
+                  style={{
+                    margin: "5px",
+                    width: "100%",
+                    borderRadius: "5px",
+                    backgroundColor: selectedAnswer === answer ? "#d3d3d3" : "white",
+                    color: "black",
+                    fontSize: "20px",
+                    padding: "5px",
+                    marginTop: "10px",
+                  }}
+                  onClick={() => handleAnswerClick(answer)}
+                >
+                  {answer}
+                </Button>
+              ))}
+            </Grid>
+
+            <div className="divider"></div>
+            <Box
+              display={"flex"}
+              justifyContent={"space-between"}
+              alignItems={"center"}
+              padding={"50px"}
+              width="100%"
+            >
+              <img
+                src="/images/logos/utp_squad.png"
+                alt="ImagenUTP"
+                style={{ overflow: "hidden", display: "block" }}
+              />
+              <Box>
+                {currentQuestionIndex < questions.length - 1 ? (
+                  <Button
+                    style={{
+                      backgroundColor: "#5138f2",
+                      color: "white",
+                      fontSize: "46px",
+                      borderRadius: "20px",
+                      padding: "10px 60px",
+                      margin: "0 10",
+                    }}
+                    variant="contained"
+                    onClick={handleNextQuestion}
+                    disabled={selectedAnswer === null}
+                  >
+                    Continuar
+                  </Button>
+                ) : (
+                  <Link href="/" passHref>
+                    <Button
+                      style={{
+                        backgroundColor: "#5138f2",
+                        color: "white",
+                        fontSize: "46px",
+                        borderRadius: "20px",
+                        padding: "10px 60px",
+                        margin: "0 10",
+                      }}
+                      variant="contained"
+                      component="a"
+                    >
+                      Continuar
+                    </Button>
+                  </Link>
+                )}
+              </Box>
+            </Box>
+          </Grid>
+        </Card>
+      </Grid>
+    </PageContainer>
+  );
+};
+
+export default PlantillaPregunta;
--- src/app/authentication/login/login_pregun/login_utepin/page.tsx
+++ src/app/authentication/login/login_pregun/login_utepin/page.tsx
@@ -10,6 +10,14 @@ import { text } from "stream/consumers";
 const Utepin = () => {
   return (
     <PageContainer title="UTEPIN" description="pagina de UTEPIN">
+      <style jsx>{`
+        .divider {
+          border-top: 1px solid #ccc;
+          margin: 2px 0;
+          width: 100%;
+        }
+      `}</style>
+
       <Grid
         container
         spacing={0}
@@ -36,11 +44,59 @@ const Utepin = () => {
             justifyContent="center"
             alignItems="center"
             textAlign="center"
+            flexDirection={"column"}
           >
-            <Typography>a</Typography>
-            <Typography>a</Typography>
-            <Typography>a</Typography>
-            <Typography>a</Typography>
+            <Typography variant="subtitle2"marginTop={"140px"}>
+              <span
+                style={{
+                  color: "#000",
+                  backgroundColor: "#a3dfea",
+                  padding: "20px 30px",
+                  borderRadius: "30px",
+                  fontSize: "40px",
+                  fontWeight: "bold",
+                  boxShadow: "0 2px 4px #eeeee4",
+                }}
+              >
+                ¡Hola! yo soy UTEPIN
+              </span>
+            </Typography>
+            <img
+              src="/images/logos/utepin.png"
+              alt="ImagenUTP"
+              style={{
+                overflow: "hidden",
+                display: "block",
+                marginTop: "40px",
+                marginBottom: "60px",
+              }}
+            />
+            <div className="divider"></div>
+            <Box
+              display={"flex"}
+              justifyContent={"space-between"}
+              alignItems={"center"}
+              padding={"50px"}
+              width="100%"
+            >
+              <img
+                src="/images/logos/utp_squad.png"
+                alt="ImagenUTP"
+                style={{ overflow: "hidden", display: "block" }}
+              />
+              <Box>
+                <Button
+                  style={{ backgroundColor: "#5138f2", color: "white" , fontSize: "46px" , borderRadius: "20px", padding: "10px 60px", margin: "0 10"}}
+                  // color="primary"
+                  variant="contained"
+                  component={Link}
+                  href="/authentication/Quest"
+                  type="submit"
+                >
+                  Continuar
+                </Button>
+              </Box>
+            </Box>
           </Grid>
         </Card>
       </Grid>
